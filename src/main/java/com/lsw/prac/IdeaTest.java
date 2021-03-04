2c4b33effeb7e748afa9c1d172fb98e676ece8e5
李绍炜 <lishaowei01@58.com>
2021-03-04 23:45:58 +0800

    test idea's language inject

---
+++ src/main/java/com/lsw/prac/IdeaTest.java
@@ -0,0 +1,17 @@
+package com.lsw.prac;
+
+public class IdeaTest {
+    public static void main(String[] args) {
+        testIdeaInject();
+    }
+
+    private static void testIdeaInject() {
+        // json
+        //language=JSON
+        String json = "{\"name\": \"daadreeam\", \"age\": 22, \"company\": {}, \"hobby\": [\"coding\", \"coding\", \"coding\"]}";
+
+        // sql
+        //language=GenericSQL
+        String sql = "select * from user where id = 1;";
+    }
+}
